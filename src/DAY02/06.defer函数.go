52efd3a96461bc52d1c56cdea1f06a9168ce429d
navigatezll <navigatezll@didichuxing.com>
2021-04-07 00:26:06 +0800

    defer的作用

--- src/DAY02/06.defer函数.go
+++ src/DAY02/06.defer函数.go
@@ -7,6 +7,8 @@ import (
 
 /**
  * 1.  延迟，关键字，用于匿名函数 和 函数调用
+ * 2.  一般defer用于退出栈的清理作用。比如清理临时变量，清理句柄。释放空间
+ * 3. 在通一函数使用 多个 defer 语句，会通过压栈的方式在最后释放阶段相反出栈。
  */
 func main() {
 	fileName := "/Users/didi/go/src/DAY02/01.switch.go"
